df924f62d2cc47630d8fa3f2dcb0fb3766b8cbec
NayaradcPinheiro <nayaradc.pinheiro@gmail.com>
2022-01-02 19:36:38 -0300

    Feito as Rotas

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
-import {useState, useEffect} from "react";
-import { getCharacters } from "./API/RickAndMorty";
-import styled from "styled-components";
+import React from "react";
+import Router from "./Routes/Router";
 
-const CharacterCard = styled.div`
-    border: 1px solid black;
-    color:black;
-    background: white;
-    padding: 10px;
-    margin: 10px;
-    width: 300px;
-   `
 
-function App() {
-  const [charactersList, setCharactersList] = useState([]);
-
-  useEffect(() => {
-    getCharacters(setCharactersList)
-  }, []);
-
-  function showCharacters() {
-    return charactersList.map((character) => {
-        return <CharacterCard>{character.name}</CharacterCard>
-    })
-  }
-    return(
-      <div>
-      <h1>The Rick and Morty</h1>
-      {showCharacters()}
-      </div>
-    );
-}
+const App = () => {
+  return (
+     <Router/>   
+  );
+};
 
 export default App;
+
---
+++ src/Routes/Coordinator.js
@@ -0,0 +1,3 @@
+export const goToNextPage = (history) => {
+    history.push("/nextPage");
+  };
